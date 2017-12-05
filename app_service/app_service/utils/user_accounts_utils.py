82b5dbe0583cf892d9edab78e31affcac6c93a36
Ubuntu <ubuntu@ip-172-31-27-31.us-west-2.compute.internal>
2017-12-05 21:03:48 +0000

    small changes

--- app_service/app_service/utils/user_accounts_utils.py
+++ app_service/app_service/utils/user_accounts_utils.py
@@ -78,7 +78,7 @@ def create_user(data):
 	# data = {'email':'bbb@gmail.com','pswd':'123','details':{'first_name':'BBB','last_name':'User 2'}} #  "X-Authorization-Token": "e46cc17e-cc4b-49e6-a7f5-bd9fe387beea"
 	# data = {'email':'ccc@gmail.com','pswd':'123','details':{'first_name':'CCC','last_name':'User 3'}} # "X-Authorization-Token": "c8bdd3b0-2277-4462-ba1d-f7a3d02da9e9"
 	headers = {'content-type':'application/json'}
-	url = 'http://ec2-18-221-144-47.us-east-2.compute.amazonaws.com/userservice/createuser/'
+	url = 'http://ec2-34-214-219-216.us-west-2.compute.amazonaws.com/userservice/createuser/'
 	response = requests.post(url,headers=headers,data=json.dumps(data))
 	print response.text
 	return response
@@ -87,7 +87,7 @@ def create_user(data):
 def login_user(email, pswd):
 	data = {'email':email,'pswd':pswd,'remember_me':'1'} 
 	headers = {'content-type':'application/json'}
-	url = 'http://ec2-18-221-144-47.us-east-2.compute.amazonaws.com/userservice/uservalidation/'
+	url = 'http://ec2-34-214-219-216.us-west-2.compute.amazonaws.com/userservice/uservalidation/'
 	response = requests.post(url,headers=headers,data=json.dumps(data))
 	#print response.text
 	return response
@@ -113,7 +113,7 @@ def populate_db():
 		key = login_user(each[0],each[1]).json()['responseData']['content']['X-Authorization-Token']
 
 		headers = {'content-type':'application/json', 'Origin':['POST','PUT','DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH'], 'X-Authorization-Token': key}
-		url = 'http://ec2-18-221-144-47.us-east-2.compute.amazonaws.com/cardservice/createcard/'
+		url = 'http://ec2-34-214-219-216.us-west-2.compute.amazonaws.com/cardservice/createcard/'
 
 		for d in data:
 			da = {}
@@ -132,4 +132,14 @@ def populate_db():
 
 
 if __name__ == "__main__":
-	populate_db()
+	print search_index_main('Java')
+	#populate_db()
+	#data = {'email':'aaa@gmail.com','pswd':'123','details':{'first_name':'AAA','last_name':'User 1'}} # "X-Authorization-Token": "089b2d64-1c0f-4457-85dc-7060e3f5fdc7"
+        #response = create_user(data)
+	#print response
+	#data = {'email':'bbb@gmail.com','pswd':'123','details':{'first_name':'BBB','last_name':'User 2'}} #  "X-Authorization-Token": "e46cc17e-cc4b-49e6-a7f5-bd9fe387beea"
+        #response = create_user(data)
+	#print response
+	#data = {'email':'ccc@gmail.com','pswd':'123','details':{'first_name':'CCC','last_name':'User 3'}} # "X-Authorization-Token": "c8bdd3b0-2277-4462-ba1d-f7a3d02da9e9"
+	#response = create_user(data)
+	#print response
